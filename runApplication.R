68c23e28730bfc0da2cd16d0bcc8c2beb07b1e45
MananLedwani <149069224+MananLedwani@users.noreply.github.com>
2024-11-15 00:39:46 +0530

    Create runApplication.R
    This is the final execution of how to run the application after designing the UI and Server.

---
+++ runApplication.R
@@ -0,0 +1,9 @@
+# Load necessary libraries
+library(shiny)
+
+# The code of ui.R and server.R to be used for running the application
+source("ui.R")
+source("server.R")
+
+# Run the application
+shinyApp(ui = ui, server = server)
